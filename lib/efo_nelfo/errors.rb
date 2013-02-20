e052391cc55b19e8222ec35c9b9d4c0e1ceabe1b
Gudleik Rasch <gudleik@gmail.com>
2013-02-20 13:25:47 +0100

    Removed unused error classes

--- lib/efo_nelfo/errors.rb
+++ lib/efo_nelfo/errors.rb
@@ -1,7 +1,4 @@
 module EfoNelfo
-  class UnsupportedVersion < StandardError; end
   class UnsupportedPostType < StandardError; end
-  class UnsupportedFileFormat < StandardError; end
-  class UnknownFileType < StandardError; end
   class DuplicateProperty < StandardError; end
 end
