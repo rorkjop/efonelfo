4e95a96e5987bbe4fd0414f51cc7eb12417de4a0
Gudleik Rasch <gudleik@gmail.com>
2013-06-20 17:02:18 +0200

    Added turn

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,9 @@
 require 'efo_nelfo'
 
+require 'turn/autorun'
 require 'minitest/spec'
 require 'minitest/autorun'
 require 'minitest/pride'
 require 'awesome_print'
+
+Turn.config.format = :progress
