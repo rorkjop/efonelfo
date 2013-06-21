d10ea7a7ea987b150a8f4c665b284dba7a343202
Gudleik Rasch <gudleik@gmail.com>
2013-06-21 09:47:05 +0200

    EfoNelfo::Array#<< accepts a hash

--- lib/efo_nelfo/array.rb
+++ lib/efo_nelfo/array.rb
@@ -10,15 +10,20 @@ module EfoNelfo
 
     def_delegators :@list, :[], :each, :last, :size, :empty?
 
-    def initialize(*args)
-      @owner     = args[0]
-      @post_type = args[1]
+    def initialize(owner, post_type)
+      @owner     = owner
+      @post_type = post_type
       @list      = []
     end
 
     def <<(obj)
+      obj = post_type_class.new(obj) if obj.is_a? Hash
       raise EfoNelfo::InvalidPostType if obj.is_a?(EfoNelfo::PostType) && obj.post_type != post_type
-      @list << obj
+      @list << obj if obj
+    end
+
+    def post_type_class
+      Kernel.const_get("EfoNelfo::V#{owner.class.version_from_class}::#{post_type}")
     end
 
     def to_a
--- spec/array_spec.rb
+++ spec/array_spec.rb
@@ -1,36 +1,67 @@
 require 'spec_helper'
 
 describe EfoNelfo::Array do
-  let(:owner) { MiniTest::Mock.new }
-  let(:type)  { MiniTest::Mock.new }
+  Owner = Class.new do
+    def self.version_from_class
+      '21'
+    end
+  end
 
-  let(:array) { EfoNelfo::Array.new "The owner", "BT" }
+  let(:owner) { Owner.new }
+  let(:array) { EfoNelfo::Array.new owner, "BT" }
 
   it "accepts owner and post_type arguments" do
-    array.owner.must_equal "The owner"
+    array.owner.must_equal owner
     array.post_type.must_equal "BT"
   end
 
   describe "<<" do
-    Foo = Class.new(EfoNelfo::PostType)
+    module EfoNelfo
+      module V21
+        class MyType < EfoNelfo::PostType
+          property :whatever
+          property :version
+        end
 
-    let(:obj) { Foo.new }
-
-    it "accepts a valid hash" do
-      obj = { post_type: "BT", whatever: 'blah' }
-      array << obj
-      array.size.must_equal 1
+        class BT < EfoNelfo::PostType
+          property :post_type
+          property :whatever
+          property :version
+        end
+      end
     end
 
-    it "accepts a valid posttype" do
-      def obj.post_type; 'BT'; end
-      array << obj
-      array.must_include obj
+    describe "passing a hash" do
+      let(:hash) {
+        { post_type: "BT", version: "2.1", whatever: 'blah' }
+      }
+
+      it "accepts a valid hash" do
+        array << hash
+        array.size.must_equal 1
+      end
+
+      it "converts the hash into a post type" do
+        array << hash
+        array[0].must_be_instance_of EfoNelfo::V21::BT
+        array[0].whatever.must_equal 'blah'
+      end
+
     end
 
-    it "raises an error if the object being added is of wrong type" do
-      def obj.post_type; 'BH'; end
-      lambda { array << obj }.must_raise(EfoNelfo::InvalidPostType)
+    describe "passing a PostType object" do
+      let(:obj) { EfoNelfo::V21::MyType.new }
+
+      it "accepts a valid posttype" do
+        def obj.post_type; 'BT'; end
+        array << obj
+        array.must_include obj
+      end
+
+      it "raises an error if the object being added is of wrong type" do
+        def obj.post_type; 'BH'; end
+        lambda { array << obj }.must_raise(EfoNelfo::InvalidPostType)
+      end
     end
   end
 
