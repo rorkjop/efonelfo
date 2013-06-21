aaddbb704cf7c39fa24c83ed5f04f8eeb918b92c
Gudleik Rasch <gudleik@gmail.com>
2013-06-21 14:06:25 +0200

    Added discount models (RH and RL)

---
+++ lib/efo_nelfo/v40/rl.rb
@@ -0,0 +1,12 @@
+module EfoNelfo
+  module V40
+    class RL < EfoNelfo::PostType
+      property :post_type,         alias: :PostType,      limit: 2,  default: post_type, required: true
+      property :product_type,      alias: :VareMrk,       limit: 1,  type: :integer, required: true
+      property :product_number,    alias: :VareNr,        limit: 14, required: true
+      property :price,             alias: :AvtaltPris,    limit: 10, type: :integer
+      property :discount,          alias: :Rabatt,        limit: 2,  required: true
+      property :text,              alias: :Tekst,         limit: 30
+    end
+  end
+end
