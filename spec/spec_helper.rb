b3be10c2e446b11f9489b404fe602d65253a39e1
Gudleik Rasch <gudleik@gmail.com>
2013-07-09 18:32:18 +0200

    Added 'its' helper to minitest

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,3 +15,16 @@ end
 def csv(filename)
   File.expand_path("../samples/#{filename}", __FILE__)
 end
+
+# Add 'its' to minitest - copied from https://github.com/agileanimal/its-minitest (gem doesn't work)
+class MiniTest::Spec
+  def self.its attribute, &block
+    describe "verify subject.#{attribute} for"  do
+      let(:inner_subject) { subject.send(attribute) }
+
+      it "verify subject.#{attribute} for" do
+        inner_subject.instance_eval &block
+      end
+    end
+  end
+end
