e88dbd0d969a3f31c0dc6bb2fb75848b3e45183d
Gudleik Rasch <gudleik@gmail.com>
2013-10-08 21:29:53 +0200

    Added decimal setting to RL#price

--- lib/efo_nelfo/v40/rl.rb
+++ lib/efo_nelfo/v40/rl.rb
@@ -4,7 +4,7 @@ module EfoNelfo
       property :post_type,         alias: :PostType,      limit: 2,  default: post_type, required: true
       property :product_type,      alias: :VareMrk,       limit: 1,  type: :integer, required: true
       property :product_number,    alias: :VareNr,        limit: 14, required: true
-      property :price,             alias: :AvtaltPris,    limit: 10, type: :integer
+      property :price,             alias: :AvtaltPris,    limit: 10, type: :integer, decimals: 2
       property :discount,          alias: :Rabatt,        limit: 2,  required: true, type: :integer, decimals: 2
       property :text,              alias: :Tekst,         limit: 30
     end
