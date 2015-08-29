4f94cecc619e53946d92a6b38d5edd17660715c0
Gudleik Rasch <gudleik@gmail.com>
2015-08-29 13:43:58 +0200

    Added CH, CL and CT post types

---
+++ lib/efo_nelfo/v40/ct.rb
@@ -0,0 +1,12 @@
+module EfoNelfo
+  module V40
+    class CT < EfoNelfo::PostType
+      property :post_type, alias: :PostType, limit: 2, default: post_type, required: true
+      property :text,      alias: :FriTekst, limit: 30
+
+      def to_s
+        text
+      end
+    end
+  end
+end
